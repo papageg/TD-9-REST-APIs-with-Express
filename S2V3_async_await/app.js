7f9571bdf9838d085815ec04f9ead6ae3cd3c517
papageg <greggordon98@gmail.com>
2019-06-10 22:15:11 -0700

    sleep
    sleep

--- S2V3_async_await/app.js
+++ S2V3_async_await/app.js
@@ -7,21 +7,34 @@ app.use(express.json());
 
 // Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res)=>{
-    const quotes = await records.getQuotes();
-    res.json(quotes);
+    try{
+        const quotes = await records.getQuotes();
+        res.json(quotes);
+    }catch(err){
+        res.json({message:err.message});
+    }
 });
 // Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', async (req, res)=>{
-    const quote = await records.getQuote(req.params.id);
-    res.json(quote);
+    try{
+        const quote = await records.getQuote(req.params.id);
+        res.json(quote);
+    }catch(err){
+        res.json({message:err.message});
+    }
 });
 // Send a POST request to /quotes to  CREATE a new quote 
 app.post('/quotes', async (req,res) => {
-    const quote = await records.createQuote({
-        quote: req.body.quote,
-        author: req.body.author
-    });
-    res.json(quote);
+    try{
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.json(quote);
+    }catch(err){
+        res.json({message:err.message});
+    }
+
 });
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // Send a DELETE request to /quotes/:id DELETE a quote 
