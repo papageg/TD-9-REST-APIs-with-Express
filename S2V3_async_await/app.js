a89a3200d803f5f6a1d04be2cbee73ba15d1a2c2
Greg <greggordon98@gmail.com>
2019-06-11 10:35:55 -0700

    starbucks break
    starbucks break

--- S2V3_async_await/app.js
+++ S2V3_async_await/app.js
@@ -18,25 +18,51 @@ app.get('/quotes', async (req, res)=>{
 app.get('/quotes/:id', async (req, res)=>{
     try{
         const quote = await records.getQuote(req.params.id);
-        res.json(quote);
+        if(quote){
+            res.json(quote);
+        } else {
+            res.status(404).json({message:"Quote not found"})
+        }
+        
     }catch(err){
-        res.json({message:err.message});
+        res.status(500).json({message:err.message});
     }
 });
 // Send a POST request to /quotes to  CREATE a new quote 
 app.post('/quotes', async (req,res) => {
     try{
-        const quote = await records.createQuote({
-            quote: req.body.quote,
-            author: req.body.author
-        });
-        res.json(quote);
+        if(req.body.author && req.body.quote){
+            const quote = await records.createQuote({
+                quote: req.body.quote,
+                author: req.body.author
+            });
+            res.status(201).json(quote);
+        } else {
+            res.status(400).json({message: "Quote and authior required"});
+        }
+
     }catch(err){
-        res.json({message:err.message});
+        res.status(500).json({message:err.message});
     }
 
 });
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+app.put("/quotes/:id", async(req,res) =>{
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if(quote){
+            quote.quote = req.body.quote;
+            quote.author = req.body.author;
+
+            await records.updateQuote(quote);
+        } else {
+            res.status(404).json({message: "Quote Not Found"});
+        }
+        
+    } catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
 // Send a DELETE request to /quotes/:id DELETE a quote 
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
 
